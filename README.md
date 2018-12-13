2efbbf34a9d19c57ce234ce9d3b39792890c2c8e
Jacky Lui <jackymlui@live.com>
2018-12-14 09:37:01 +1000

    writing to newData

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Instructions
 
 1. import Tallest Buildings.csv into index.js into array
 2. extract building, country, and height (m) data 
-3. export this extract into a new file to be saved
-4. get tallest building
+3. export this extract into a new csv file to be saved
+4. get mean height building
 5. get country with most tallest buildings 
--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 
 const fs = require('fs');
 const parse = require('csv-parse');
+const stringify = require('csv-stringify');
+
 let readStream = fs.createReadStream('Tallest Buildings.csv');
 let writeStream = fs.createWriteStream('newData.csv');
 
 let csvArr = [];
 // read into csv Arr
-
+let checkFirstRow = true;
 // what does pipe do? what does on do?
 readStream.pipe(parse({delimiter:','})).on('data', (csvrow)=>{ 
     // csvArr.push(csvrow);
-    // only push csvrows[1,3,& 4]
-    let newCsvRow = [csvrow[1], csvrow[3], csvrow[4]];
-    csvArr.push(newCsvRow);
+    if (checkFirstRow) {
+        console.log("skipping first row");
+        checkFirstRow = false;
+    }
+    else{ // only push csvrows[1,3,& 4]
+        let newCsvRow = [csvrow[1], csvrow[3], csvrow[4]];
+        csvArr.push(newCsvRow);
+        writeStream.write(newCsvRow.toString() + ",\r\n");
+    }
 }).on('end', ()=>{
     console.log(csvArr);
-    
+    writeStream.end();
 });
 
-const func = ()=>{
-    console.log("running func");
-    // remove header
-}
+// how do these anonymous functions work?
+// const removeHeader = ()=>{ 
+//     // remove header
+//     csvArr = csvArr.slice(1);
+// }
 
+// const writeCSVToStream = ()=>{
+//     csvArr.forEach((row)=>{
+//         writeStream.pipe(stringify({delimiter:','}).write(row));
+//     })
+// }
